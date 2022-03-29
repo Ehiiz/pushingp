6db78c691a45eb60f92bbe12be9ba575a4a93ad4
Ehiiz <62038343+Ehiiz@users.noreply.github.com>
2022-03-29 10:00:28 +0100

    added environment variable

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const encrypt = require('mongoose-encryption')
 
 const app = express();
 
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,9 +20,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret"
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema)
 
